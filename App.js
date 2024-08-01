53fa2ba969c74f8b5c052b093c06a4651de7f83f
Emma <152114708+Donner-Kabab@users.noreply.github.com>
2024-07-31 20:21:33 -0600

    practiced text and alert buttons

--- App.js
+++ App.js
@@ -1,9 +1,47 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TextInput, View, Text, Alert, Button } from "react-native";
+import { useState } from "react";
 
 const App = () => {
-  return <View></View>;
+  const [text, setText] = useState("");
+  // alert the user input (`text` state's value)
+  const alertMyText = () => {
+    Alert.alert(text);
+  };
+  return (
+    <View>
+      <TextInput
+        style={styles.TextInput}
+        value={text}
+        onChangeText={setText}
+        placeholder="Type Something Here"
+      />
+      <Text>You wrote: {text}</Text>
+      <Button
+        onPress={() => {
+          alertMyText();
+        }}
+        title="Press Me"
+      />
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textInput: {
+    width: "88%",
+    borderWidth: 1,
+    height: 50,
+    padding: 10,
+  },
+  textDisplay: {
+    height: 50,
+    lineHeight: 50,
+  },
+});
 
 export default App;
