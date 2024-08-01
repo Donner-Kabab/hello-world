e1c7dc181ef1b03ea842454b1972af3ae31abe58
Emma <152114708+Donner-Kabab@users.noreply.github.com>
2024-08-01 16:37:48 -0600

    created screen1 and screen2 to prep for different pages

--- App.js
+++ App.js
@@ -1,47 +1,27 @@
 import { StyleSheet, TextInput, View, Text, Alert, Button } from "react-native";
 import { useState } from "react";
+import Screen1 from './components/Screen1';
+import Screen2 from './components/Screen2';
 
+// Create the navigator
+const Stack = createNativeStackNavigator();
 const App = () => {
-  const [text, setText] = useState("");
-  // alert the user input (`text` state's value)
-  const alertMyText = () => {
-    Alert.alert(text);
-  };
   return (
-    <View>
-      <TextInput
-        style={styles.TextInput}
-        value={text}
-        onChangeText={setText}
-        placeholder="Type Something Here"
-      />
-      <Text>You wrote: {text}</Text>
-      <Button
-        onPress={() => {
-          alertMyText();
-        }}
-        title="Press Me"
-      />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Screen1"
+      >
+        <Stack.Screen
+          name="Screen1"
+          component={Screen1}
+        />
+        <Stack.Screen
+          name="Screen2"
+          component={Screen2}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  textInput: {
-    width: "88%",
-    borderWidth: 1,
-    height: 50,
-    padding: 10,
-  },
-  textDisplay: {
-    height: 50,
-    lineHeight: 50,
-  },
-});
+}
 
 export default App;
