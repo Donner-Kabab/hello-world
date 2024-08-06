ac7d3664b9c47b8dc76cbaa8fe9fd8df57a25d35
Emma <152114708+Donner-Kabab@users.noreply.github.com>
2024-08-06 17:20:38 -0600

    installed gifted chat and made chat.js file easier to use

--- components/Chat.js
+++ components/Chat.js
@@ -1,15 +1,38 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
+import { StyleSheet, View, Text, KeyboardAvoidingView, Platform } from "react-native";
 
 const Chat = ({ route, navigation }) => {
+  const [messages, setMessages] = useState([]);
   const { name, backgroundColor } = route.params;
 
   useEffect(() => {
     navigation.setOptions({ title: name });
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
   }, []);
 
   return (
-    <View style={[styles.container, { backgroundColor: backgroundColor }]}></View>
+    <View style={[styles.container, { backgroundColor: backgroundColor }]}>
+      <GiftedChat
+        messages={messages}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1
+        }}
+      />
+      { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
+    </View>
   );
 };
 
